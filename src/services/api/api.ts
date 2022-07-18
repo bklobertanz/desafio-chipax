2ef6960bc76a96b2210e52659955f4d37e07125b
Barís Klobertanz <klobertanz@gmail.com>
2022-07-18 01:07:53 -0400

    chore: move api

---
+++ src/services/api/api.ts
@@ -0,0 +1,30 @@
+import { StatusCodes } from 'http-status-codes'
+
+const BASE_URL = 'https://rickandmortyapi.com/api'
+
+const fetchAll = async <T>(resource: string, pageNumber?: number) => {
+  const url = pageNumber ? `${BASE_URL}/${resource}/?page=${pageNumber}` : `${BASE_URL}/${resource}`
+
+  const requestInit: RequestInit = {
+    method: 'GET'
+  }
+
+  const result = await fetch(url, requestInit)
+  if (result.status !== StatusCodes.OK) console.log(`${result.status}: ${result.statusText}`)
+
+  const data = await result.json()
+  return <T>data
+}
+const fetchOne = async <T>(url: string) => {
+  const requestInit: RequestInit = {
+    method: 'GET'
+  }
+
+  const result = await fetch(url, requestInit)
+  if (result.status !== StatusCodes.OK) {
+    console.log(`${result.status}: ${result.statusText}`)
+  }
+  const data = await result.json()
+  return <T>data
+}
+export default { fetchAll, fetchOne }
