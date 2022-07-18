8110ad1281701b1d4d699d742d6273cd473d184f
Barís Klobertanz <klobertanz@gmail.com>
2022-07-17 20:54:50 -0400

    feat(api): add fetchOne fx

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 
 const BASE_URL = 'https://rickandmortyapi.com/api'
 
-const fetchAll = async (resource: string, pageNumber?: number) => {
+const fetchAll = async <T>(resource: string, pageNumber?: number) => {
   const url = pageNumber ? `${BASE_URL}/${resource}/?page=${pageNumber}` : `${BASE_URL}/${resource}`
 
   const requestInit: RequestInit = {
@@ -13,7 +13,19 @@ const fetchAll = async (resource: string, pageNumber?: number) => {
   if (result.status !== StatusCodes.OK) throw new Error(`${result.status}: ${result.statusText}`)
 
   const data = await result.json()
-  return data
+  return <T>data
 }
+const fetchOne = async <T>(url: string) => {
+  const requestInit: RequestInit = {
+    method: 'GET'
+  }
 
-export default { fetchAll }
+  const result = await fetch(url, requestInit)
+  if (result.status !== StatusCodes.OK) {
+    console.log(result.headers)
+    throw new Error(`${result.status}: ${result.statusText}`)
+  }
+  const data = await result.json()
+  return <T>data
+}
+export default { fetchAll, fetchOne }
