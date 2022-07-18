db65e77ff56e12be24a583219fbc0191b6b2bf45
Barís Klobertanz <klobertanz@gmail.com>
2022-07-18 01:06:39 -0400

    refactor: move helper fx to utils

--- src/helpers/utils.ts
+++ src/helpers/utils.ts
@@ -1,6 +1,27 @@
+import { resourceNameType } from '../types/types'
+
 export const calcPerformanceTimeInSec = (initialTime: number, finalTime: number) => {
   const ONE_SEC_IN_MS = 1000
   return (finalTime - initialTime) / ONE_SEC_IN_MS
 }
 
-export default calcPerformanceTimeInSec
+export const isInTime = (performanceTime: number) => {
+  const thresholdTimeInS = 3
+  return performanceTime < thresholdTimeInS
+}
+
+export const generateAnswer = (
+  exerciseName: string,
+  time: number,
+  results: Record<string, string | number>[]
+) => ({
+  exercise_name: exerciseName,
+  time,
+  in_time: isInTime(time),
+  results
+})
+export const generateResults = (letter: string, count: number, resource: resourceNameType) => ({
+  char: letter,
+  count,
+  resource
+})
