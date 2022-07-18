d995481a0809b0d022bffaec14b455e4c45e2ed2
Barís Klobertanz <klobertanz@gmail.com>
2022-07-17 23:59:46 -0400

    chore: improve console logging

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import executeExerciseOne from './exercises/exerciseOne'
 import { EXERCISE_ONE_NAME } from './common/constants'
 
-Promise.all([executeExerciseOne(EXERCISE_ONE_NAME)]).then((results) => console.log('hola', results))
+Promise.all([executeExerciseOne(EXERCISE_ONE_NAME)]).then((results) =>
+  console.log('hola', JSON.stringify(results, undefined, 2))
+)
