d3d7b2ace094554ce0dc7ab2aa036722c2ac1425
Barís Klobertanz <klobertanz@gmail.com>
2022-07-18 19:10:27 -0400

    refactor(ex2): clean execution fx

--- src/exercises/exerciseTwo.ts
+++ src/exercises/exerciseTwo.ts
@@ -3,30 +3,46 @@ import { Character, Episode } from '../types/types'
 import { getAllResources } from '../services/resources'
 import { calcPerformanceTimeInSec, generateAnswer } from '../helpers/utils'
 
-const executeExerciseTwo = async (exerciseName: string) => {
-  const initialTime = performance.now()
+const getEpisodeLocations = (
+  episodeCharacters: string[],
+  charactersOriginMap: Map<string, string>
+): string[] =>
+  Array.from(
+    new Set(
+      episodeCharacters.map(
+        (episodeCharacter: string) => charactersOriginMap.get(episodeCharacter)!
+      )
+    )
+  )
+const getAllData = async () => {
   const resourcePromises = [
     getAllResources<Character>(resourceNames.character),
     getAllResources<Episode>(resourceNames.episode)
   ]
-  const [charResults, epiResults] = await Promise.all(resourcePromises)
+  return Promise.all(resourcePromises)
+}
+const generateHelper = (characters: Character[]) =>
+  new Map(characters.map((character: Character) => [character.url, character.origin.name]))
+
+const getEpisodesLocations = (episodes: Episode[], charactersOriginMap: Map<string, string>) =>
+  episodes.map(({ name, episode, characters: episodeCharacters }) => ({
+    name,
+    episode,
+    locations: getEpisodeLocations(episodeCharacters, charactersOriginMap)
+  }))
+
+const executeExerciseTwo = async (exerciseName: string) => {
+  const initialTime = performance.now()
+
+  const [charResults, epiResults] = await getAllData()
+
   const characters = charResults as Character[]
   const episodes = epiResults as Episode[]
 
-  const charactersOriginMap = new Map(
-    characters.map((character: Character) => [character.url, character.origin.name])
-  )
-  const episodesLocations = episodes.map(({ name, episode, characters: episodeCharacters }) => ({
-    name,
-    episode,
-    locations: Array.from(
-      new Set(
-        episodeCharacters.map(
-          (episodeCharacter: string) => charactersOriginMap.get(episodeCharacter)!
-        )
-      )
-    )
-  }))
+  const charactersOriginMap = generateHelper(characters)
+
+  const episodesLocations = getEpisodesLocations(episodes, charactersOriginMap)
+
   const finalTime = performance.now()
 
   const performanceTimeInSeconds = calcPerformanceTimeInSec(initialTime, finalTime)
