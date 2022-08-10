0dee44208f394b4e72049988c9b9c3dae325ec2e
Barís Klobertanz <klobertanz@gmail.com>
2022-08-10 14:04:13 -0400

    refactor(exercise): improve code readability

--- src/exercises/exerciseOne.ts
+++ src/exercises/exerciseOne.ts
@@ -1,77 +1,8 @@
+/* eslint-disable no-use-before-define */
 import { resourceNames, resourceNameLetter } from '../enums/resources'
 import { calcPerformanceTimeInSec, generateAnswer } from '../helpers/utils'
 import { getAllResources } from '../services/resources'
 import { Character, Episode, Location, resourceNameType } from '../types/types'
-
-const getAllResourcesData = (): Promise<(Character[] | Episode[] | Location[])[]> => {
-  const resourcePromises = [
-    getAllResources<Character>(resourceNames.character),
-    getAllResources<Episode>(resourceNames.episode),
-    getAllResources<Location>(resourceNames.location)
-  ]
-  return Promise.all(resourcePromises)
-}
-
-const getAllResourcesNames = (
-  characters: Character[],
-  episodes: Episode[],
-  locations: Location[]
-): string[][] => {
-  const charactersNames = characters.map((character: Character) => character.name)
-  const episodesNames = episodes.map((episode: Episode) => episode.name)
-  const locationsNames = locations.map((location: Location) => location.name)
-  return [charactersNames, episodesNames, locationsNames]
-}
-
-export const countWordByLetter = (word: string, letter: string): number => {
-  const regExp = new RegExp(letter, 'gi')
-  return Number(word.match(regExp)?.length) || 0
-}
-
-export const wordsCounter = (words: string[], letter: string): number =>
-  words
-    .map((word: string) => countWordByLetter(word, letter))
-    .reduce((a: number, b: number) => a + b)
-
-const generateResults = (
-  letter: string,
-  count: number,
-  resource: resourceNameType
-): Record<string, string | number> => ({
-  char: letter,
-  count,
-  resource
-})
-const countWordsAndGetResult = (
-  resourceNames: string[],
-  resourceName: resourceNameType,
-  resourceLetter: string
-): Record<string, string | number> => {
-  const totalLetters = wordsCounter(resourceNames, resourceLetter)
-  return generateResults(resourceLetter, totalLetters, resourceName)
-}
-const countResourcesNamesAndResult = (
-  charactersNames: string[],
-  locationsNames: string[],
-  episodesNames: string[]
-): Record<string, string | number>[] => {
-  const characterResults = countWordsAndGetResult(
-    charactersNames,
-    resourceNames.character,
-    resourceNameLetter[resourceNames.character]
-  )
-  const locationResults = countWordsAndGetResult(
-    locationsNames,
-    resourceNames.location,
-    resourceNameLetter[resourceNames.location]
-  )
-  const episodeResults = countWordsAndGetResult(
-    episodesNames,
-    resourceNames.episode,
-    resourceNameLetter[resourceNames.episode]
-  )
-  return [characterResults, locationResults, episodeResults]
-}
 
 export const executeExerciseOne = async (
   exerciseName: string
@@ -106,3 +37,74 @@ export const executeExerciseOne = async (
     episodeResults
   ])
 }
+const getAllResourcesData = (): Promise<(Character[] | Episode[] | Location[])[]> => {
+  const resourcePromises = [
+    getAllResources<Character>(resourceNames.character),
+    getAllResources<Episode>(resourceNames.episode),
+    getAllResources<Location>(resourceNames.location)
+  ]
+  return Promise.all(resourcePromises)
+}
+
+const getAllResourcesNames = (
+  characters: Character[],
+  episodes: Episode[],
+  locations: Location[]
+): string[][] => {
+  const charactersNames = characters.map((character: Character) => character.name)
+  const episodesNames = episodes.map((episode: Episode) => episode.name)
+  const locationsNames = locations.map((location: Location) => location.name)
+  return [charactersNames, episodesNames, locationsNames]
+}
+
+const countResourcesNamesAndResult = (
+  charactersNames: string[],
+  locationsNames: string[],
+  episodesNames: string[]
+): Record<string, string | number>[] => {
+  const characterResults = countWordsAndGetResult(
+    charactersNames,
+    resourceNames.character,
+    resourceNameLetter[resourceNames.character]
+  )
+  const locationResults = countWordsAndGetResult(
+    locationsNames,
+    resourceNames.location,
+    resourceNameLetter[resourceNames.location]
+  )
+  const episodeResults = countWordsAndGetResult(
+    episodesNames,
+    resourceNames.episode,
+    resourceNameLetter[resourceNames.episode]
+  )
+  return [characterResults, locationResults, episodeResults]
+}
+
+const countWordsAndGetResult = (
+  resourceNames: string[],
+  resourceName: resourceNameType,
+  resourceLetter: string
+): Record<string, string | number> => {
+  const totalLetters = wordsCounter(resourceNames, resourceLetter)
+  return generateResults(resourceLetter, totalLetters, resourceName)
+}
+
+export const wordsCounter = (words: string[], letter: string): number =>
+  words
+    .map((word: string) => countWordByLetter(word, letter))
+    .reduce((a: number, b: number) => a + b)
+
+export const countWordByLetter = (word: string, letter: string): number => {
+  const regExp = new RegExp(letter, 'gi')
+  return Number(word.match(regExp)?.length) || 0
+}
+
+const generateResults = (
+  letter: string,
+  count: number,
+  resource: resourceNameType
+): Record<string, string | number> => ({
+  char: letter,
+  count,
+  resource
+})
--- src/exercises/exerciseTwo.ts
+++ src/exercises/exerciseTwo.ts
@@ -1,38 +1,8 @@
+/* eslint-disable no-use-before-define */
 import { resourceNames } from '../enums/resources'
 import { Character, Episode } from '../types/types'
 import { getAllResources } from '../services/resources'
 import { calcPerformanceTimeInSec, generateAnswer } from '../helpers/utils'
-
-const getEpisodeLocations = (
-  episodeCharacters: string[],
-  charactersOriginMap: Map<string, string>
-): string[] =>
-  Array.from(
-    new Set(
-      episodeCharacters.map(
-        (episodeCharacter: string) => charactersOriginMap.get(episodeCharacter)!
-      )
-    )
-  )
-const getAllResourcesData = async (): Promise<(Character[] | Episode[])[]> => {
-  const resourcePromises = [
-    getAllResources<Character>(resourceNames.character),
-    getAllResources<Episode>(resourceNames.episode)
-  ]
-  return Promise.all(resourcePromises)
-}
-const generateHelper = (characters: Character[]): Map<string, string> =>
-  new Map(characters.map((character: Character) => [character.url, character.origin.name]))
-
-const getEpisodesLocations = (
-  episodes: Episode[],
-  charactersOriginMap: Map<string, string>
-): Record<string, string | string[]>[] =>
-  episodes.map(({ name, episode, characters: episodeCharacters }) => ({
-    name,
-    episode,
-    locations: getEpisodeLocations(episodeCharacters, charactersOriginMap)
-  }))
 
 const executeExerciseTwo = async (
   exerciseName: string
@@ -53,5 +23,38 @@ const executeExerciseTwo = async (
   const performanceTimeInSeconds = calcPerformanceTimeInSec(initialTime, finalTime)
   return generateAnswer(exerciseName, performanceTimeInSeconds, episodesLocations)
 }
+
+const getAllResourcesData = async (): Promise<(Character[] | Episode[])[]> => {
+  const resourcePromises = [
+    getAllResources<Character>(resourceNames.character),
+    getAllResources<Episode>(resourceNames.episode)
+  ]
+  return Promise.all(resourcePromises)
+}
+
+const generateHelper = (characters: Character[]): Map<string, string> =>
+  new Map(characters.map((character: Character) => [character.url, character.origin.name]))
+
+const getEpisodesLocations = (
+  episodes: Episode[],
+  charactersOriginMap: Map<string, string>
+): Record<string, string | string[]>[] =>
+  episodes.map(({ name, episode, characters: episodeCharacters }) => ({
+    name,
+    episode,
+    locations: getEpisodeLocations(episodeCharacters, charactersOriginMap)
+  }))
+
+const getEpisodeLocations = (
+  episodeCharacters: string[],
+  charactersOriginMap: Map<string, string>
+): string[] =>
+  Array.from(
+    new Set(
+      episodeCharacters.map(
+        (episodeCharacter: string) => charactersOriginMap.get(episodeCharacter)!
+      )
+    )
+  )
 
 export default executeExerciseTwo
